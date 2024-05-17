1fbab9d3e0f3f787ba37dbba3a366be3f18354c7
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2024-05-17 20:36:08 +0300

    Add files via upload

---
+++ hw_09_05_2024/hw_09_05_2024/hw_09_05_2024.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <stdio.h>
+using namespace std;
+
+// Напишите программу для расчёта оплаты за электроэнергию.
+// Создайте базовый класс для всех типов счетчиков электроэнергии, 
+// имеющий метод для вычисления стоимости потребленной электроэнергии.
+// Реализуйте несколько подклассов счетчиков, например, обычный, двухтарифный, многотарифный и т.д.
+// Переопределите метод для каждого подкласса так, чтобы он корректно вычислял 
+// стоимость в соответствии с тарифами.Создайте вектор указателей на объекты базового класса и заполните его объектами 
+// каждого из подклассов.Напишите функцию для вывода стоимости потребленной электроэнергии для каждого счетчика
+
+class Meter
+{
+protected:
+    float kwH;
+public:
+    Meter()
+    {
+        kwH = rand() % 100 + 1;
+    }
+
+    float GetKwH() const
+    {
+        return kwH;
+    }
+
+    virtual float Calculate() = 0;
+};
+
+class MechanicalMeter : public Meter
+{
+public:
+    MechanicalMeter() : Meter() {}
+
+    virtual float Calculate()
+    {
+        float price = 1.67;
+        return kwH * price;
+    }
+};
+
+class DayNightMeter : public Meter
+{
+private:
+    float dayKwH;
+    float nightKwH;
+public:
+    DayNightMeter()
+    {
+        dayKwH = rand() % 500 + 1;
+        nightKwH = rand() % 500 + 1;
+        kwH = dayKwH + nightKwH;
+    }
+
+    virtual float Calculate()
+    {
+        float dayPriceKwH = 1.67;
+        float nightPriceKwH = 1.15;
+        return (dayKwH * dayPriceKwH) + (nightKwH * nightPriceKwH);
+    }
+};
+
+__interface IMPrinter {
+    virtual void Print(Meter* c) = 0;
+};
+
+class ConsolePrinter : public IMPrinter {
+public:
+
+    void Print(Meter* c) {
+        cout << "Price: " << c->Calculate() << " UAH" << "\n";
+    }
+};
+
+int main()
+{
+    srand(time(NULL));
+
+    MechanicalMeter m;
+    DayNightMeter d;
+    ConsolePrinter printer;
+
+    vector<Meter*> meters;
+    meters.push_back(&m);
+    meters.push_back(&d);
+
+    for (int i = 0; i < meters.size(); i++) {
+        printer.Print(meters[i]);
+    }
+}
